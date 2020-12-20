15a526288d22923bc6dff9bc9037e35ab86a2496
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2020-12-20 21:10:35 +0545

    Email template tweaks

--- src/Emails/templates/template.php
+++ src/Emails/templates/template.php
@@ -2,17 +2,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>A Simple Responsive HTML Email</title>
+        <title><?php echo get_blog_info();?> </title>
         <style type="text/css">
         body {margin: 0; padding: 0; min-width: 100%!important;}
         .content {width: 100%; max-width: 600px;}  
         </style>
     </head>
-    <body yahoo bgcolor="#f6f8f1">
+    <body>
         <table width="100%" bgcolor="#f6f8f1" border="0" cellpadding="0" cellspacing="0">
             <tr>
                 <td>
-                    <table class="content" align="center" cellpadding="0" cellspacing="0" border="0">
+                    <table class="content" cellpadding="0" cellspacing="0" border="0">
                         <tr>
                             <td style="padding: 20px">
                                <?php echo $message;?>
