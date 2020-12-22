f603d3a811cf1e1768b5f199b551dab9bb6f257c
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2020-12-22 19:17:25 +0545

    Improve template file comments

--- src/Emails/templates/template.php
+++ src/Emails/templates/template.php
@@ -1,3 +1,23 @@
+<?php
+/**
+ * The Email template.
+ *
+ * This template can be overridden by copying it to yourtheme/come-back/template.php.
+ *
+ * HOWEVER, on occasion Come Back! will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://sanjeebaryal.com.np/come-back-template-structure
+ *
+ * @version 1.3.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -15,7 +35,8 @@
                     <table class="content" cellpadding="0" cellspacing="0" border="0">
                         <tr>
                             <td style="padding: 20px">
-                               <?php echo $message;?>
+                               <?php echo $message;     // The message body that is inserted in the Settings > Come Back! > Email Message.
+                               ?>
                             </td>
                         </tr>
                     </table>
